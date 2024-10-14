2247da3594d15450cc4b22070b1d04604262edfb
Danu <dciptadi@gmail.com>
2024-10-14 16:50:56 +0700

    update 14-10-2024

--- migrate.php
+++ migrate.php
@@ -9,24 +9,24 @@ $dbNew = $GLOBALS['db_new'];
 
 include('src/helper_func.php');
 
-// include('src/migration/tbl_kategori_belanja.php');
-// include('src/migration/users.php');
-// include('src/migration/tbl_ppk2.php');
+include('src/migration/tbl_kategori_belanja.php');
+include('src/migration/users.php');
+include('src/migration/tbl_ppk2.php');
 
-// include('src/migration/tbl_cabang_ut.php');
-// include('src/migration/tbl_profile_penyedia.php');
-// include('src/migration/tbl_paket.php');
+include('src/migration/tbl_cabang_ut.php');
+include('src/migration/tbl_profile_penyedia.php');
+include('src/migration/tbl_paket.php');
  
-// include('src/migration/tbl_domisili.php');
+include('src/migration/tbl_domisili.php');
 
-// include('src/migration/tbl_pengalaman_perorangan.php');
-// include('src/migration/tbl_sertif_perorangan.php');
-// include('src/migration/tbl_komisaris_perusahaan.php');
-// include('src/migration/tbl_direksi_perusahaan.php');
-// include('src/migration/tbl_personalia_perusahaan.php');
-// include('src/migration/tbl_ijin_usaha_perusahaan.php');
-// include('src/migration/tbl_saham_perusahaan.php');
-// include('src/migration/tbl_akta_perusahaan.php');
+include('src/migration/tbl_pengalaman_perorangan.php');
+include('src/migration/tbl_sertif_perorangan.php');
+include('src/migration/tbl_komisaris_perusahaan.php');
+include('src/migration/tbl_direksi_perusahaan.php');
+include('src/migration/tbl_personalia_perusahaan.php');
+include('src/migration/tbl_ijin_usaha_perusahaan.php');
+include('src/migration/tbl_saham_perusahaan.php');
+include('src/migration/tbl_akta_perusahaan.php');
 include('src/migration/tbl_pengalaman.php');
 include('src/migration/tbl_pengalaman_sekarang.php');
 include('src/migration/tbl_fasilitas_perusahaan.php');
